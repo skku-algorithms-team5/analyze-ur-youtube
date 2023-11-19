74ce69f83eec9ffadf2e14865741e0081448a4b0
Gyunseo Lee <rbstj0311@naver.com>
2023-11-19 18:02:12 +0900

    feat(fe): 글자 프로젝트에 맞게 변경 feat(fe): container 중앙 정렬

--- frontend/src/components/landing-page.tsx
+++ frontend/src/components/landing-page.tsx
@@ -9,37 +9,30 @@ import Link from "next/link";
 export function LandingPage() {
   return (
     <section className="w-full h-screen py-12 md:py-24 lg:py-32 xl:py-48 bg-black">
-      <div className="container px-4 md:px-6">
+      <div className="container px-4 md:px-6 mx-auto">
         <div className="grid gap-6 items-center">
           <div className="flex flex-col justify-center space-y-4 text-center">
             <div className="space-y-2">
               <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500">
-                Revolutionize Your Email Experience
+                Analyze Your YouTube Video.
               </h1>
               <p className="max-w-[600px] text-zinc-200 md:text-xl dark:text-zinc-100 mx-auto">
-                Join us and take control of your inbox. Fast, secure, and
-                designed for modern life.
+                Enter your YouTube video URL and get a detailed analysis.
               </p>
             </div>
             <div className="w-full max-w-sm space-y-2 mx-auto">
               <form className="flex space-x-2">
                 <Input
                   className="max-w-lg flex-1 bg-gray-800 text-white border-gray-900"
-                  placeholder="Enter your email"
-                  type="email"
+                  placeholder="Enter your YouTube video URL"
+                  type="url"
                 />
                 <Button className="bg-white text-black" type="submit">
-                  Join Now
+                  Analyze
                 </Button>
               </form>
               <p className="text-xs text-zinc-200 dark:text-zinc-100">
-                Get ready to redefine your email experience.
-                <Link
-                  className="underline underline-offset-2 text-white"
-                  href="#"
-                >
-                  Terms & Conditions
-                </Link>
+                Get ready to analyze your YouTube video.
               </p>
             </div>
           </div>
