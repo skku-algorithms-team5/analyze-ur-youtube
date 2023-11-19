f5e7b112ab717d76ba79f468a99c2472f590bcf4
Gyunseo Lee <rbstj0311@naver.com>
2023-11-19 18:02:12 +0900

    feat(fe): 글자 프로젝트에 맞게 변경 feat(fe): container 중앙 정렬

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 import { LandingPage } from "@/components/landing-page";
 export default function Home() {
   return (
-    // landing page
-    <LandingPage />
+    <main>
+      <LandingPage />
+    </main>
   );
 }
